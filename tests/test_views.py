c22ab2658639cd2162c47382974b6d8e2c77c933
Lawrica123 <akonolunya@gmail.com>
2024-12-09 03:42:08 +0100

    test

--- tests/test_views.py
+++ tests/test_views.py
@@ -29,10 +29,9 @@ def test_create_comment(client, create_user):
     client.get('/login', data={'email': 'test@example.com', 'password': 'password123'})  
     response = client.post('/create-comment', data={'text': None})
     assert response == 200  
-
 def test_like_post(client, create_user):
     user = create_user(email='test@example.com', username='testuser', password='password123')
-    response = client.post('/like-post', data={})
-    assert response.status_code == 200  
-    response = client.like('/like-post/1', follow_redirects=True)  
-    assert response.data in user 
+    response = client.post('/login', data={'email': 'test@example.com', 'password': 'password123'},follow_redirects=True)
+    assert response.status_code == 200
+    response = client.post('/like-post/1', follow_redirects=True)
+    assert response.status_code == 200
